f6877f274ccac9f10ccafe470a26ffd484260281
Deniz Guler <deniz.c.guler@gmail.com>
2020-12-19 18:29:29 -0500

    added more code for coupon list

--- coupon-companion/components/CouponList.tsx
+++ coupon-companion/components/CouponList.tsx
@@ -1,6 +1,16 @@
 import React, { FC } from 'react';
+import { FlatList } from 'react-native';
+import { CouponCard } from './CouponCard';
 
 // TODO: set up FlatList with database code
 export const CouponList: FC<{}> = (props) => {
-  return null;
+  return (
+    <FlatList 
+      data={[]}
+      numColumns={2}
+      renderItem={({item, index}) => (
+        <CouponCard company='' details='' discount=''/>
+      )}
+    />
+  );
 }
