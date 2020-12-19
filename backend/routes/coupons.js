4a2f7526e016abb57f4664a9011ab650942fcc11
Valeriy Soltan <vsoltan@umass.edu>
2020-12-19 16:59:49 -0500

    added endpoints for coupon collection

--- backend/routes/coupons.js
+++ backend/routes/coupons.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const User = require('../models/user'); 
 
 const { isLoggedIn } = require('../middleware/verification');
+const { isValidCoupon } = require('../middleware/validation'); 
 
 router.get("/", (req, res, next) => {
     res.send("Hi from Coupons");
@@ -20,11 +21,29 @@ router.get("/all", isLoggedIn, async (req, res, next) => {
     return res.status(200).send(authenticatedUser.coupons);
 }); 
 
-router.post("/add", (req, res, next) => {
+router.post("/add", isLoggedIn, (req, res, next) => {
     if (req.body.coupon === undefined) {
         res.status(422).send("Please supply a coupon object to add"); 
     }
-    
+    if (!isValidCoupon(req.body.coupon)) {
+        res.status(422).send("Coupon object is improperly formatted!"); 
+    }
+    User.findOneAndUpdate(
+        { _id: req.user._id }, 
+        { $push: { 
+            coupons: {
+                $each: [ req.body.coupon ], 
+                $sort: { companyName: 1 }
+            }
+        }}, 
+        (err, result) => {
+            if (err) {
+                res.status(400).send("Updating coupon collection error: " + err.message); 
+            } else {
+                res.status(200).send("Updated coupon collection successfully!"); 
+            }
+        }
+    );
 }); 
 
 module.exports = router;
